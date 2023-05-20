3cd26da65dbec2a3b286d8bf4c6a74f08d93450b
Schrodawg <cjschroe@ucsc.edu>
2023-05-19 20:08:41 -0700

    Level 1 working (I hope)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ///SCENE 0
-class initial extends Phaser.Scene {
+class initial extends SkateScene {
     constructor() {
         super('intial');
     }
@@ -19,7 +19,7 @@ class initial extends Phaser.Scene {
     }
 
 ///SCENE 1
-class intro extends Phaser.Scene {
+class intro extends SkateScene {
     constructor() {
         super('intro');
     }
@@ -78,33 +78,103 @@ class intro extends Phaser.Scene {
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
                 this.scene.start('level1')
             })
-        }, 8000);
+        }, 9000);
     }
     update(){}
     }
 
 ///Level 1
-class level1 extends Phaser.Scene {
+class level1 extends SkateScene {
     constructor() {
         super('level1');
     }
-    preload(){}
-    create() {
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('board', 'skateboard.png');
+        this.load.image('pipe', 'pipe.png');
+        this.load.image('tunnel', 'tunnel.png');
+    }
+    create(time) {
         //Canvas size
         this.w = this.game.config.width;
         this.h = this.game.config.height;
-        //
-        const startButton = this.add.text(this.w * .45, this.h * .45, 'Click Here?', { fill: '#ffffff' })
-          .setInteractive()
-          .on('pointerdown', () => {
-            this.scene.start('intro')
-        })   
+        //Pipes
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(230, 200, 'pipe').setDisplaySize(400,10).refreshBody()
+            .setSize(400, 10, true);
+        platforms.create(550, 645, 'pipe').setDisplaySize(this.w,10).refreshBody()
+            .setSize(this.w, 10, true);
+        
+        //Board
+        this.board = this.physics.add.sprite(100, 0, 'board').setDisplaySize(600,250)
+        .setSize(575, 250, true)
+        //Tunnel    
+        this.tunnel = this.physics.add.staticGroup();
+        this.tunnel.create(100, 500, 'tunnel').setDisplaySize(600, 300)
+            .setSize(200, 400)
+        //Physics
+        this.board.setBounce(0.2);
+        this.board.setCollideWorldBounds(true);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.board, platforms);
+        this.physics.add.collider(
+            this.board,
+            this.tunnel, 
+            (board, tunnel) =>
+            {
+                if (board.body.touching.left && tunnel.body.touching.right)
+                {
+                    let levelTime = this.game.getTime();
+                    this.recordTime(levelTime)
+                    this.gotoScene('level1Sum');
+                }
+            });
       }
-    update(){}
+    update() {
+        const { left, right, up } = this.cursors;
+
+        if (left.isDown)
+        {
+            this.board.setVelocityX(-160);
+        }
+        else if (right.isDown)
+        {
+            this.board.setVelocityX(160);
+        }
+        else
+        {
+            this.board.setVelocityX(0);
+        }
+
+        if (up.isDown && this.board.body.touching.down)
+        {
+            this.board.setVelocityY(-330);
+        }
+        
     }
+}
 
-
-
+///Level 1 Summary
+class level1Sum extends SkateScene {
+    constructor() {
+        super('level1Sum');
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('board', 'skateboard.png');
+    }
+    create() {
+        this.sumText = this.add.text(
+            300, 
+            300,
+            `YOU BEAT THAT IN: ${this.time * .001} seconds!`,
+            {
+                font: "italic 20px Pacifico",
+                color: "#ffffff",
+            }
+        )
+    }
+}
 
 
 window.onload = function(){
@@ -115,7 +185,14 @@ window.onload = function(){
         type: Phaser.WEBGL,
         width: 1920*.6,
         height: 1080*.6,
-        scene: [intro],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: true
+            }
+        },
+        scene: [level1, level1Sum],
     }
     let game = new Phaser.Game(config);
 }
---
+++ skate.js
@@ -0,0 +1,39 @@
+class SkateScene extends Phaser.Scene {
+
+    init(data) {
+        this.time = data.time || [];
+        this.actualTime = data.actualTime || 0;
+        console.log(this.time)
+        console.log('INIT: ', this.actualTime)
+    }
+
+    constructor(key) {
+        super(key);
+    }
+
+    create() {
+        this.transitionDuration = 1000;
+
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        this.s = this.game.config.width * 0.01;
+
+    }
+
+    recordTime(levelTime) {
+        console.log(levelTime);
+        this.time.push(levelTime);
+        this.actualTime = 0;
+        this.actualTime = this.time[0];
+        this.time.push(this.actualTime);
+        console.log('ActualTime: ', this.actualTime);
+    }
+
+    gotoScene(key) {
+        this.cameras.main.fadeOut(1000, 0, 0, 0);
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start(key)
+            })
+        this.scene.start(key, { time: this.actualTime });
+        };
+}
