65f67c8f995c8d44e851a8dfd97f9be2a042e845
Schrodawg <cjschroe@ucsc.edu>
2023-05-19 20:39:36 -0700

    Working final version

--- script.js
+++ script.js
@@ -290,7 +290,148 @@ class level2Sum extends SkateScene {
     }
 }
 
+///Level 3
+class level3 extends SkateScene {
+    constructor() {
+        super('level3');
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('board', 'skateboard.png');
+        this.load.image('pipe', 'pipe.png');
+        this.load.image('tunnel', 'tunnel.png');
+    }
+    create(time) {
+        //Canvas size
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        //Pipes
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(200, 170, 'pipe').setDisplaySize(600,10).refreshBody()
+            .setSize(600, 10, true);
+        platforms.create(900, 170, 'pipe').setDisplaySize(400,10).refreshBody()
+            .setSize(400, 10, true);
+        platforms.create(550, 645, 'pipe').setDisplaySize(this.w,10).refreshBody()
+            .setSize(this.w, 10, true);
+        
+        //Board
+        this.board = this.physics.add.sprite(100, 50, 'board').setDisplaySize(600,250)
+        .setSize(575, 250, true)
+        //Tunnel    
+        this.tunnel = this.physics.add.staticGroup();
+        this.tunnel.create(625, 700, 'tunnel').setDisplaySize(600, 300)
+            .setSize(400, 370)
+            .setAngle(270);
+        //Physics
+        this.board.setBounce(0.2);
+        this.board.setCollideWorldBounds(true);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.board, platforms);
+        this.physics.add.collider(
+            this.board,
+            this.tunnel, 
+            (board, tunnel) =>
+            {
+                if (board.body.touching.down && tunnel.body.touching.up)
+                {
+                    let levelTime = this.game.getTime();
+                    this.recordTime(levelTime)
+                    this.gotoScene('level3Sum');
+                }
+            });
+      }
+    update() {
+        const { left, right, up } = this.cursors;
 
+        if (left.isDown)
+        {
+            this.board.setVelocityX(-160);
+        }
+        else if (right.isDown)
+        {
+            this.board.setVelocityX(160);
+        }
+        else
+        {
+            this.board.setVelocityX(0);
+        }
+
+        if (up.isDown && this.board.body.touching.down)
+        {
+            this.board.setVelocityY(-300);
+        }
+        
+    }
+}
+
+///Level 3 Summary
+class level3Sum extends SkateScene {
+    constructor() {
+        super('level3Sum');
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('board', 'skateboard.png');
+    }
+    create() {
+        this.sumText = this.add.text(
+            300, 
+            300,
+            `YOU BEAT THAT IN: ${this.time * .001} seconds!`,
+            {
+                font: "italic 20px Pacifico",
+                color: "#ffffff",
+            }
+        )
+    // Fade to black TIMED
+    setTimeout(() => {
+        this.cameras.main.fadeOut(1000, 0, 0, 0);
+        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+            this.gotoScene('fin')
+        })
+    }, 8000);
+    }
+}
+
+///End
+class fin extends SkateScene {
+    constructor() {
+        super('fin');
+    }
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('board', 'skateboard.png');
+    }
+    create() {
+        this.sumText = this.add.text(
+            450, 
+            300,
+            "GREAT JOB, YOU MADE IT!",
+            {
+                font: "italic 20px Pacifico",
+                color: "#ffffff",
+            }
+        )
+        let board = this.add.image(-200, 500, 'board').setScale(.3);
+        //Animation
+        this.tweens.add({
+            targets: board,
+            x: 600,
+            delay: 1500,
+            duration: 3000,
+            ease: 'Quad.easeOut',
+        })
+        this.tweens.add({
+            targets: board,
+            angle: 360*10,
+            yoyo: true,
+            flipX: true,
+            duration: 3000, 
+            delay: 4300,
+            ease: 'linear',
+        }); 
+    }
+}
 
 window.onload = function(){
     let config = {
@@ -307,7 +448,7 @@ window.onload = function(){
                 debug: true
             }
         },
-        scene: [level2, level2Sum],
+        scene: [initial, intro, level1, level1Sum, level2, level2Sum,level3, level3Sum, fin],
     }
     let game = new Phaser.Game(config);
 }
